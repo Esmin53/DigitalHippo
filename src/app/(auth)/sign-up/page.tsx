e7128233175c25f6e05c9bbb898de7dc42395eaf
Esmin53 <esmin.tufekcic53@gmail.com>
2024-03-10 23:51:22 +0100

    tRPC setup

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -11,6 +11,7 @@ import {useForm} from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 
 import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator"
+import { trpc } from "@/trpc/client"
 
 const page = () => {
 
@@ -21,6 +22,7 @@ const page = () => {
             formState: { errors } } = useForm<TAuthCredentialsValidator>({
                 resolver: zodResolver(AuthCredentialsValidator)
             })
+
 
     const onSubmit = ({email, password}: TAuthCredentialsValidator) => {
 
