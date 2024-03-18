7e2fc7cedec6b9f55a17cdd613e40667e814a0c3
Esmin53 <esmin.tufekcic53@gmail.com>
2024-03-18 01:16:48 +0100

    Final security tweaks

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,7 +1,7 @@
 import { BeforeChangeHook, AfterChangeHook } from "payload/dist/collections/config/types";
 import { PRODUCT_CATEGORIES } from "../../config";
-import { CollectionConfig } from "payload/types";
-import { Product } from "../../payload-types";
+import { Access, CollectionConfig } from "payload/types";
+import { Product, User } from "../../payload-types";
 import { stripe } from "../../lib/stripe";
 
 
@@ -45,15 +45,46 @@ const syncUser: AfterChangeHook<Product> = async ({
   }
 }
 
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined
+
+    if (!user) return false
+    if (user.role === 'admin') return true
+
+    const userProductIDs = (user.products || []).reduce<
+      Array<string>
+    >((acc, product) => {
+      if (!product) return acc
+      if (typeof product === 'string') {
+        acc.push(product)
+      } else {
+        acc.push(product.id)
+      }
+
+      return acc
+    }, [])
+
+    return {
+      id: {
+        in: userProductIDs,
+      },
+    }
+  }
+
 export const Products: CollectionConfig = {
     slug: 'products',
     admin: {
         useAsTitle: 'name'
     },
     access: {
-
+      read: isAdminOrHasAccess()
     },
     hooks: {
+      afterChange: [
+        syncUser
+      ],
       beforeChange: [
         addUser,
         async (args) => {
